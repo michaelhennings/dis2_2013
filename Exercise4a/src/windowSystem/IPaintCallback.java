386c96a06a3d7fcdfdb42c34ed087c4500808639
Michael Hennings <mwh_github@hotmail.de>
2013-06-08 16:03:48 +0200

    Removed wrong files

--- Exercise4a/src/windowSystem/IPaintCallback.java
+++
@@ -1,8 +0,0 @@
-package windowSystem;
-
-/**
- *  Callback interface which is used to draw inside windows.
- */
-public interface IPaintCallback {
-    public void handleDraw(DrawingContext drawingContext);
-}
